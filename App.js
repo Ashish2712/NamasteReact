27ad9a502f869d14223b94dd7f3b158ba21918b1
Ashish <akushwah206@gmail.com>
2025-03-13 22:06:08 +0530

    added code for siblings

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading = React.createElement("h1", {}, "hello world from react!");
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading", xyz:"abc"},
+//     "hello world from react!");
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
